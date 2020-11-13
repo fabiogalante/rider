88e7ee741935c0fcc5c1f5684a87c009fa901d81
mans <fabio.mans@grupoboticario.com.br>
2020-11-13 14:40:18 -0300

    Initial commit

---
+++ WebApp/Controllers/Users.Users.cs
@@ -0,0 +1,7 @@
+namespace WebApp.Controllers
+{
+    public class Users
+    {
+        public int Test { get; set; }
+    }
+}
---
+++ WebApp/Model/Aluno.cs
@@ -0,0 +1,7 @@
+namespace WebApp.Model
+{
+    public class Aluno
+    {
+        
+    }
+}
