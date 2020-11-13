a6ec047162dab949763ec4cd7f345a2d3fe0d428
mans <fabio.mans@grupoboticario.com.br>
2020-11-13 15:26:02 -0300

    Aluno

--- WebApp/Model/Aluno.cs
+++ WebApp/Model/Aluno.cs
@@ -2,6 +2,19 @@ namespace WebApp.Model
 {
     public class Aluno
     {
-        
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public Aluno()
+        {
+            
+        }
+
+        public void Teste()
+        {
+            
+        }
+       
     }
 }
